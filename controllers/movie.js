52848eb5815a982ebcb9572b8ad7cf54bb4676f5
Kevin Macwan <kevinmacwn2612@gmail.com>
2022-05-09 12:20:10 +0530

    Error handling

--- controllers/movie.js
+++ controllers/movie.js
@@ -1,24 +1,31 @@
 const { check, getMovie, createMovie } = require("./movieController")
 
 const create = async (req, res) => {
-  const user = req.user
-  if (!req.body.title) {
-    return res.status(400).send({ message: "Title Required!" });
-  }
-  const [canAdd, List] = await check({ user })
-  if (canAdd) {
-    const movieCreateRes= await createMovie({title:req.body.title,user})
-    if (movieCreateRes===null) {
-      res.status(500).send({
-        message: 'some thing went wrong'
-      })
-    } else {
-      res.send({ message: 'movie created', movieCreateRes })
+  try {
+    const user = req.user
+    if (!req.body.title) {
+      return res.status(400).send({ message: "Title Required!" });
     }
-  } else {
+    const [canAdd, List] = await check({ user })
+    if (canAdd) {
+      const movieCreateRes = await createMovie({ title: req.body.title, user })
+      if (movieCreateRes === null) {
+        res.status(500).send({
+          message: 'some thing went wrong'
+        })
+      } else {
+        res.send({ message: 'movie created', movieCreateRes })
+      }
+    } else {
+      res.status(500).send({
+        message: 'User can not create more then 5 movies'
+      })
+    }
+  } catch (error) {
     res.status(500).send({
-      message: 'User can not create more then 5 movies'
+      message: error.message
     })
+
   }
 };
 
--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,15 +1,16 @@
 const { movie } = require('../model/model')
 const Omdbapi = require('../OMDBAPI')
-async function check({user}){
-    const list = await getMovie({user})
-    if ((userMovies.length < 5 && user.role === 'basic') || user.role === 'Premium') {
-    return [true,list]
-    }
-    return [false] 
+async function check({ user }) {
+  const list = await getMovie({ user })
+  if ((userMovies.length < 5 && user.role === 'basic') || user.role === 'Premium') {
+    return [true, list]
+  }
+  return [false]
 
 }
 
-async function createMovie({title,user}){
+async function createMovie({ title, user }) {
+  try {
 
     const movieRes = await Omdbapi.getByTitle(title);
     const { Title, Released, Genre, Director } = movieRes;
@@ -24,30 +25,32 @@ async function createMovie({title,user}){
 
     const movieDB = new movie(movieData)
     movieDB.save(movieDB).then(data => {
-        console.log('in sucess',data);
-        return data
-      }).catch(error => {
-        console.log('in fail',error);
+      return data
+    }).catch(error => {
 
-       return null
-      })
+      return null
+    })
+  } catch (error) {
+    throw error
+  }
+
 }
-async function getMovie ({user}) {
-    const currentDate = new Date();
-    const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-    const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-     return userMovies = await movie.find({
-        userId: user.userId,
-        createdAt: {
-          $gte: firstDayOfMonth,
-          $lte: lastDayOfMonth
-        }
-      });
+async function getMovie({ user }) {
+  const currentDate = new Date();
+  const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+  const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+  return userMovies = await movie.find({
+    userId: user.userId,
+    createdAt: {
+      $gte: firstDayOfMonth,
+      $lte: lastDayOfMonth
+    }
+  });
 }
 
-async function deleteMovieBytitle ({titles}) {
-   return userMovies = await movie.deleteMany({
-      title:{'$in':titles}
-    });
+async function deleteMovieBytitle({ titles }) {
+  return userMovies = await movie.deleteMany({
+    title: { '$in': titles }
+  });
 }
-module.exports={check,createMovie,getMovie,deleteMovieBytitle}
+module.exports = { check, createMovie, getMovie, deleteMovieBytitle }
